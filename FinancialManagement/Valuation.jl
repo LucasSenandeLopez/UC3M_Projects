46bd12bbcc20f25d4fa3ad1e2bd53ffcbc12d1e4
LucasSenandeLopez <lsenandelopez03@gmail.com>
2024-04-23 13:09:00 +0200

    Advancements on project

--- FinancialManagement/Valuation.jl
+++ FinancialManagement/Valuation.jl
@@ -3,23 +3,17 @@ using Main.DCF
 using Plots
 using LaTeXStrings
 
-const D = 29332000;
-const cash = 22927000;
-const n_acciones = 2407000;
-const market_premium = 0.1782;
-const r_f = 0.0388;
-const βᵤ = 0.3051;
-const β_d = (0.0059/market_premium);
-const rd = 0.0453 * (1 - 0.3);
+const D = 29_332_000.0;  
+const cash = 22_927_000.0; 
+const n_acciones = 2_407_000; 
+const market_premium = 0.1532;
+const r_f = 0.0388; #Cogemos el de 2023 porque los datos del CAPM se obtuvieron de ese año
+const βᵤ = 0.3076;
+const β_d = 0.0045;
+const rd = 0.0453 * (1 - 0.3); #Descontamos ya los impuestos
 
-#re::Float64 = 0.0978;
-
-rw::Float64 = 0.0902;
+rw::Float64 = 0.0829;
 g::Float64 = 0.035;
-
-
-
-
 
 function get_wacc(d_v)
 
@@ -34,7 +28,7 @@ function get_wacc(d_v)
 end
 
 # Datos sacados del excel en esta misma carpeta
-aft_ebit::Vector{Float64} = [11580197.41, 12270615.47,12940843.57, 13582975.70, 14189055.92, 14751234.51 ];
+aft_ebit::Vector{Float64} = [11580197.41, 12270615.47,12940843.57, 13582975.70, 14189055.92, 14751234.51];
 
 amort::Vector{Float64} = [7945742.374, 8419472.12, 8879348.55, 9319946.96, 9735808.37, 10121546.72];
 
@@ -53,8 +47,9 @@ println("El precio estimado de J&J es: $(round(price, digits = 2))\$")
 prices::Matrix{Float64} = Matrix{Float64}(undef, 130, 130);
 
 
-waccs = [0.0902 + 0.0009 * i for i in -49:80];
-g_rates = [0.035 + 0.0001 * i for i in -49:80];
+waccs = [0.0829 + 0.0008 * i for i in -49:80];
+g_rates = [0.03 + 0.0001 * i for i in -49:80];
+
 
 for col in 1:130
     for row in 1:130
@@ -65,7 +60,9 @@ for col in 1:130
     end
 end
 
-p_1 = plot(waccs, prices[:, 50], alpha = prices ./ 200.0 , color = :Red , label = "Precio por CCMP", area = true)
+
+# g = 3,5% es g_rates[100]
+p_1 = plot(waccs, prices[:, 100], alpha = prices ./ 200.0 , color = :Red , label = "Precio por CCMP", area = true)
 title!(L"Análisis de sensibilidad - Precio = $f(r_w)$")
 xlabel!("CCMP")
 ylabel!("Precio")
@@ -78,6 +75,8 @@ ylabel!("Precio")
 p_3 = surface(g_rates, waccs, prices, label = "Análisis de sensibilidad", title = L"Análisis de sensibilidad - Precio = $F(g, r_w)$")
 xlabel!("g");
 ylabel!("CCMP");
+xflip!(true);
+yflip!(true);
 
 savefig(p_1, "C:\\Users\\goomb\\OneDrive\\Documentos\\GitHub\\UC3M_Projects\\FinancialManagement\\SensitivityWACC.png");
 
@@ -90,7 +89,7 @@ savefig(p_3, "C:\\Users\\goomb\\OneDrive\\Documentos\\GitHub\\UC3M_Projects\\Fin
 for i in 0.2:0.05:0.4
     rw_050 = get_wacc(i) 
 
-    println("El precio si se subiera el apalancamiento al $(i * 100)% serían: ",
+    println("El precio si se subiera el ratio de endeudamiento D/V al $(i * 100)% serían: ",
     round((dcf_model(aft_ebit, capex, amort, nwc_change, rw_050, 0.04) - D + cash) / n_acciones, digits = 2),
     "\$");
 end
